b9c9cdbfa08df026af3537e2151f96bc7035227f
DankohloIvan <accs16387@gmail.com>
2025-08-14 12:49:44 +0300

    Fixed commit

--- Matrix.h
+++ Matrix.h
@@ -10,122 +10,173 @@ private:
     T** data;
     int rows;
     int cols;
-
-    void allocateMemory() {
-        data = new T * [rows];
-        for (int i = 0; i < rows; ++i)
-            data[i] = new T[cols];
-    }
-
-    void freeMemory() {
-        for (int i = 0; i < rows; ++i)
-            delete[] data[i];
-        delete[] data;
-    }
-
 public:
-    Matrix(int r, int c) : rows(r), cols(c) {
-        allocateMemory();
+    Matrix(int rows, int cols) {
+        this->rows = rows;
+        this->cols = cols;
+
+        data = new T * [rows];
+        for (int i = 0; i < rows; ++i) {
+            data[i] = new T[cols]{};
+        }
     }
 
-    ~Matrix() {
-        freeMemory();
-    }
+    Matrix(const Matrix& other) {
+        rows = other.rows;
+        cols = other.cols;
 
-    Matrix(const Matrix& other) : rows(other.rows), cols(other.cols) {
-        allocateMemory();
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
+        data = new T * [rows];
+        for (int i = 0; i < rows; ++i) {
+            data[i] = new T[cols];
+            for (int j = 0; j < cols; ++j) {
                 data[i][j] = other.data[i][j];
+            }
+        }
     }
 
     Matrix& operator=(const Matrix& other) {
-        if (this != &other) {
-            freeMemory();
-            rows = other.rows;
-            cols = other.cols;
-            allocateMemory();
-            for (int i = 0; i < rows; ++i)
-                for (int j = 0; j < cols; ++j)
-                    data[i][j] = other.data[i][j];
+        if (this == &other) {
+            return *this;
         }
+
+        for (int i = 0; i < rows; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+
+        rows = other.rows;
+        cols = other.cols;
+
+        data = new T*[rows];
+        for (int i = 0; i < rows; ++i) {
+            data[i] = new T[cols];
+            for (int j = 0; j < cols; ++j) {
+                data[i][j] = other.data[i][j];
+            }
+        }
+
         return *this;
     }
 
-    void fillFromInput() {
-        std::cout << "Enter elements:\n";
-        for (int i = 0; i < rows; ++i)
+    ~Matrix() {
+        for (int i = 0; i < rows; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    void input() {
+        for (int i = 0; i < rows; ++i) {
+            std::cout << "Enter: " << i + 1 << '#' << " row:\n";
             for (int j = 0; j < cols; ++j) {
-                std::cout << "[" << i << "][" << j << "]: ";
+                std::cout << "    Enter: " << j + 1 << '#' << " column: ";
                 std::cin >> data[i][j];
             }
+        }
     }
 
-    void fillRandom(T minVal = 0, T maxVal = 100) {
-        std::srand(static_cast<unsigned>(std::time(0)));
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
-                data[i][j] = static_cast<T>(minVal + rand() % (maxVal - minVal + 1));
-    }
-
-    void print() const {
+    void fillRandom() {
+        std::srand(std::time(0));
         for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j)
-                std::cout << data[i][j] << " ";
+            for (int j = 0; j < cols; ++j) {
+                data[i][j] = std::rand() % 100;
+            }
+        }
+    }
+
+    void print() {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                std::cout << data[i][j] << ' ';
+            }
             std::cout << '\n';
         }
     }
 
-    T getMax() const {
-        T maxVal = data[0][0];
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
-                if (data[i][j] > maxVal)
-                    maxVal = data[i][j];
-        return maxVal;
-    }
-
-    T getMin() const {
-        T minVal = data[0][0];
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
-                if (data[i][j] < minVal)
-                    minVal = data[i][j];
-        return minVal;
-    }
-
     Matrix operator+(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols) {
+            return Matrix<T>(0, 0);
+        }
         Matrix result(rows, cols);
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
                 result.data[i][j] = data[i][j] + other.data[i][j];
+            }
+        }
         return result;
     }
 
     Matrix operator-(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols) {
+            return Matrix<T>(0, 0);
+        }
         Matrix result(rows, cols);
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
                 result.data[i][j] = data[i][j] - other.data[i][j];
+            }
+        }
         return result;
     }
 
     Matrix operator*(const Matrix& other) const {
+        if (cols != other.rows) {
+            return Matrix(0, 0);
+        }
+
         Matrix result(rows, other.cols);
-        for (int i = 0; i < rows; ++i)
+        for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < other.cols; ++j) {
                 result.data[i][j] = 0;
-                for (int k = 0; k < cols; ++k)
+                for (int k = 0; k < cols; ++k) {
                     result.data[i][j] += data[i][k] * other.data[k][j];
+                }
             }
+        }
         return result;
     }
 
-    Matrix operator/(T scalar) const {
+    Matrix<T> operator/(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols) {
+            return Matrix<T>(0, 0);
+        }
         Matrix result(rows, cols);
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
-                result.data[i][j] = data[i][j] / scalar;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = data[i][j] / other.data[i][j];
+            }
+        }
         return result;
     }
+
+    T findMax() const {
+        if (rows == 0 || cols == 0) {
+            return T();
+        }
+
+        T maxVal = data[0][0];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (data[i][j] > maxVal)
+                    maxVal = data[i][j];
+            }
+        }
+        return maxVal;
+    }
+
+    T findMin() const {
+        if (rows == 0 || cols == 0) {
+            return T();
+        }
+
+        T minVal = data[0][0];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (data[i][j] < minVal)
+                    minVal = data[i][j];
+            }
+        }
+        return minVal;
+    }
+
 };
--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include "Matrix.h"
 
 int main() {
-    Matrix<int> A(2, 3);
-    Matrix<int> B(2, 3);
+    Matrix<int> matrix_1(3, 3);
+    Matrix<int> matrix_2(3, 3);
+    std::cout << "Fill 1# matrix:\n";
+    matrix_1.input();
+    matrix_1.print();
+    std::cout << "Fill 2# matrix:\n";
+    matrix_2.input();
+    matrix_2.print();
+    std::cout << '\n';
+    std::cout << "Operation +: \n";
+    Matrix<int> summa = matrix_1 + matrix_2;
+    summa.print();
 
-    A.fillRandom(1, 9);
-    B.fillRandom(1, 9);
+    std::cout << "Operation -: \n";
+    Matrix<int> difference = matrix_1 - matrix_2;
+    difference.print();
 
-    std::cout << "Matrix A:\n";
-    A.print();
+    std::cout << "Operation *: \n";
+    Matrix<int> product = matrix_1 * matrix_2;
+    product.print();
 
-    std::cout << "Matrix B:\n";
-    B.print();
-
-    Matrix<int> C = A + B;
-    std::cout << "A + B:\n";
-    C.print();
-
-    std::cout << "Max in A: " << A.getMax() << "\n";
-    std::cout << "Min in B: " << B.getMin() << "\n";
+    std::cout << "Operation /: \n";
+    Matrix<int> division = matrix_1 / matrix_2;
+    division.print();
 
     return 0;
 }
